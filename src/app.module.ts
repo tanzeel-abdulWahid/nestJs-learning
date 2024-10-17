24d0aa8580b41df021ac27e1ed437c64c10dffd8
Tanzeel <65041070+tanzeel-abdulWahid@users.noreply.github.com>
2024-10-18 02:52:36 +0500

    connected to Db using typeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,21 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [UsersModule, PostsModule, AuthModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    entities: [],
+    synchronize: true, //should only be used in dev mode, coz its recreated dB every time
+    port: 5432,
+    username: 'postgres',
+    password: '1234',
+    host: 'localhost',
+    database: 'nestjs-blog'
+
+
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
