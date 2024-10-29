12114190f96b5e38a20e06fe3a7ce1ae67798afa
Tanzeel <65041070+tanzeel-abdulWahid@users.noreply.github.com>
2024-10-30 02:26:55 +0500

    make one to many relation with user n posts

--- src/posts/post.entity.ts
+++ src/posts/post.entity.ts
@@ -1,8 +1,9 @@
-import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 import { PostType } from "./enums/PostType.enum";
 import { PostStatus } from "./enums/PostStatus.enum";
 import { CreateArticleMetaOptionsDto } from "../meta-options/dtos/create-article-meta-options.dto";
 import { MetaOption } from "src/meta-options/meta-option.entity";
+import { User } from "src/users/user.entity";
 
 @Entity()
 export class Post {
@@ -67,6 +68,8 @@ export class Post {
     })
     publishedOn?: Date;
 
+    @ManyToOne(() => User, (user) => user.posts) //foreign key will reside here--always in child entity
+    author: User;
 
     tags?: string[];
 
